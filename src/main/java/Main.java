58d2bcc863b01dbe95078414f9a1441c7fc67db2
Ilya Shiyan <ilya.shiyan97@mail.ru>
2022-11-28 07:59:34 +0300

    Finished project

---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        TreeSet<String> words = new TreeSet<>();
+        words.add("Java");
+        words.add("Program");
+        words.add("Word");
+        words.add("Apple");
+        words.add("Answer");
+        words.add("Boss");
+        words.add("Circle");
+        words.add("Moment");
+        words.add("Magic");
+        words.add("Car");
+        words.add("Animal");
+        words.add("Book");
+        words.add("Cake");
+        words.add("Cook");
+        words.add("Cool");
+        words.add("Snow");
+        words.add("Winter");
+        words.add("Play");
+        words.add("Snake");
+        words.add("Rabbit");
+
+        System.out.println(words);
+    }
+}
